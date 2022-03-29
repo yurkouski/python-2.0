80fb907d8803cd9d304dba08c1ef2c6f250fce63
Vasili.Y <Yurkovskij.V@gmail.com>
2022-03-30 01:22:56 +0300

    Add file

--- lesson - 2.py
+++ lesson - 2.py
@@ -1,6 +1,19 @@
-#1: Создать список
+#1: Создать список и проверить тип данных
 my_list = [1, 2, 3, 4, 5, "name", 13.2, "over", -145]
 print(my_list)
 for x in my_list:
     print(type(x))
 
+# Реализация обмена
+a = int(input('Введите количество элементов в вашем списке: '))
+my_list_2 = []
+b = 0
+x = 0
+while b < a:
+    my_list_2.append(input("Наберите следующий элемент списка: "))
+    b += 1
+print(my_list_2)
+while x < a/2:
+    my_list_2[x], my_list_2[x+1] = my_list_2[x+1], my_list_2[x]
+    x += 2
+print(my_list_2)
