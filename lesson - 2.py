886619ae38c2422df2984badb0a7747431a71dab
Vasili.Y <Yurkovskij.V@gmail.com>
2022-03-31 04:02:14 +0300

    Add file

--- lesson - 2.py
+++ lesson - 2.py
@@ -19,10 +19,10 @@ while x < a/2:
 print(my_list_2)
 
 #4 Пользователь вводит строку
-line = input("Введите предложение из нескольких строк с пробелами: ").split()
-print(line)
+line_1 = input("Введите предложение из нескольких строк с пробелами: ").split()
+print(line_1)
 i = 0
-for word in line:
+for word in line_1:
     i += 1
     word.count("")
     b = word.count("")
@@ -30,3 +30,23 @@ for word in line:
         print(i, word)
     else:
         print(i, word[0:10])
+
+#5 Рейтинг
+line = list(input("Введите любое количество натуральных чисел: "))
+line.sort(), line.reverse()
+print("Рейтинг: " + str(line))
+x = "stop"
+while line[0] != x:
+    z = line.append(input("Добавьте еще значение, если нет, напишите, stop: "))
+    if line[-1] == x:
+        line.remove("stop")
+        break
+        print("Рейтинг: " + str(line))
+    elif line[-1] != x:
+        line.sort(), line.reverse()
+        print("Рейтинг: " + str(line))
+        for y in range(len(line)):
+            if y == z:
+                line[y], line[z] = line[z], line[y]
+print("Рейтинг: " + str(line))
+
