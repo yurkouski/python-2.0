d8df70e067f0c8a0b5e60e2e61474ca988f2163c
Vasili.Y <Yurkovskij.V@gmail.com>
2022-04-03 20:36:47 +0300

    Add file

--- lesson - 3.py
+++ lesson - 3.py
@@ -9,6 +9,35 @@ def r_calc():
         except ZeroDivisionError:
             print("На ноль делить нельзя")
 
-
 res = r_calc()
 print(res)
+
+#2 Выполнить функцию, которая принимает несколько параметров
+def user_def():
+    name = input('Введите ваше имя: ')
+    surname = input('Введите вашу фамилию: ')
+    age = input('Введите дату рождения')
+    cyti = input('Где вы проживаете: ')
+    email = input('Введите вашу почту: ')
+    telephone = input("Введите выш телефон: ")
+    return(f"Данные пользователя: {name}, {surname}, {age} г.р., проживающий в г. {cyti}, {email}, {telephone} ")
+
+print(user_def())
+
+# 3 Задание
+def my_func(a, b, c):
+    if a >= b >= c:
+        x = a + b
+    elif a >= c >= b:
+        x = a + c
+    elif b >= a >= c:
+        x = b + a
+    elif b >= c >= a:
+        x = b + c
+    elif c >= a >= b:
+        x = c + a
+    elif c >= b >= a:
+        x = c + b
+    return(x)
+
+print(f"Результат: {my_func(int(input('Введите 1 значение: ')), int(input('Введите второе значение: ')), int(input('Введите третье значение: ')))} ")
