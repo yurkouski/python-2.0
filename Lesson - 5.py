0944c054bb137887eb9cd27eb62760d4554b7c55
Vasili.Y <Yurkovskij.V@gmail.com>
2022-04-09 18:27:48 +0300

    Add file

--- Lesson - 5.py
+++ Lesson - 5.py
@@ -11,3 +11,26 @@ file.close()
 file = open('test.txt', 'r')
 f = file.readlines()
 print(f)
+
+#2 Выполнить подсчёт строк и слов в каждой строке:
+with open(r"C:\Users\Вася\Desktop\test.txt") as file:
+    f = file.read()
+    print(f)
+
+with open(r"C:\Users\Вася\Desktop\test.txt") as file:
+    f = file.read()
+    f = f.split()
+    f = len(f)
+    print("Количество слов:" + str(f))
+
+with open(r"C:\Users\Вася\Desktop\test.txt") as file:
+    z = file.readlines()
+    z = len(z)
+    print("Количество строк:" + str(z))
+
+with open(r"C:\Users\Вася\Desktop\test.txt") as file:
+    y = 1
+    for line in file:
+        x = len(line.split())
+        print(f'В {y} строке {x} слова')
+        y +=1
