6607857d7b0cbe8a5b8824eff087e61ca0a2a4a7
Vasili.Y <Yurkovskij.V@gmail.com>
2022-04-12 22:08:07 +0300

    Add file

--- Lesson - 6.py
+++ Lesson - 6.py
@@ -1,11 +1,11 @@
 # Светофор
 from time import sleep
 class TrafficLight:
-    color = ['Red', 'yellow', 'green']
+    _color = ['Red', 'yellow', 'green']
     def running(self):
         i = 0
         while 3 > i:
-            print(f'Цвет светофора: {TrafficLight.color[i]} ')
+            print(f'Цвет светофора: {TrafficLight._color[i]} ')
             if i == 0:
                 sleep(7)
             if i == 1:
@@ -16,3 +16,15 @@ class TrafficLight:
 
 a = TrafficLight()
 a.running()
+
+# 2 Реализовать класс роад
+class Road:
+    def __init__(self, lenght, width):
+        self._lenght = lenght
+        self._width = width
+
+    def massa(self):
+        return f'Масса асфальта: {self.lenght * self.width * 25 * 5}'
+
+a = Road(10, 10)
+print(a.massa())
